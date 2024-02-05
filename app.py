e9bd86120397b40b3487bb2d378f963311203f93
Dean Morin <morin7414@gmail.com>
2024-02-04 22:30:44 -0600

    Port

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ DB_HOST = os.getenv('DB_HOST')
 DB_DATABASE = os.getenv('DB_DATABASE')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_PORT: int(os.getenv('DB_PORT', 5432))
 
 
 # Connect to PostgreSQL database
