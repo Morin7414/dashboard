9d96ca30c629724baa723eeaa87a788a679394fb
Dean Morin <morin7414@gmail.com>
2024-02-04 22:32:50 -0600

    prot

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ DB_HOST = os.getenv('DB_HOST')
 DB_DATABASE = os.getenv('DB_DATABASE')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_PORT: int(os.getenv('DB_PORT', 5432))
+DB_PORT = int(os.getenv('DB_PORT', 5432))
 
 
 # Connect to PostgreSQL database
@@ -21,7 +21,8 @@ conn = psycopg2.connect(
     host=DB_HOST,
     database=DB_DATABASE,
     user=DB_USER,
-    password=DB_PASSWORD
+    password=DB_PASSWORD,
+    port=DB_PORT
 )
 
 cursor = conn.cursor()
